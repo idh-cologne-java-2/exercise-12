54296f763b18270f4947583e7aaf2960cd41eac9
Luka <Luka@DESKTOP-74NA0L6.fritz.box>
2022-06-29 21:45:26 +0200

    completed exercise.
    added the csv maven dependency just to see if it would work and it did

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -3,7 +3,9 @@ package idh.java;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.StringTokenizer;
 
 import org.apache.commons.io.FileUtils;
@@ -28,7 +30,45 @@ public class Document implements Iterable<String> {
 	}
 	
 	public void printStats(File f) {
-		// TODO: Implement
+		String retValue = "Anzahl der Wörter,"
+				+ "Anzahl der Types,"
+				+ "Anzahl der Wörter die kürzer sind als 5 Zeichen,"
+				+ "Häufigkeit des Wortes \"blood\","
+				+ "Am häufigsten vorkommendes Wort,"
+				+ "Am häufigsten vorkommendes großgeschriebenes Wort\n";
+		int[] numericalStats = new int[] {0,0,0,0};
+		HashMap<String,Integer> frequencyMap = new HashMap<String,Integer>();
+		int mfwFrequency = 0;
+		int mfcwFrequency = 0;
+		String mostFrequentWord = new String();
+		String mostFrequentCapitalizedWord = new String();
+		for (String word : this) {
+			numericalStats[0]++;
+			if (!frequencyMap.containsKey(word))  {
+				numericalStats[1]++;
+				frequencyMap.put(word, 1);
+			}
+			else {
+				frequencyMap.put(word, frequencyMap.get(word)+1);
+			}
+			if (word.length() < 5) numericalStats[2]++;
+			if (word.equals("blood")) numericalStats[3]++;
+		}
+		for (int i : numericalStats) {
+			retValue += i + ",";
+		}
+		for (Entry<String, Integer> entry : frequencyMap.entrySet()) {
+			if (entry.getValue() > mfwFrequency) {
+				mfwFrequency = entry.getValue();
+				mostFrequentWord = entry.getKey();
+			}
+			if (Character.isUpperCase(entry.getKey().charAt(0)) && entry.getValue() > mfcwFrequency) {
+				mfcwFrequency = entry.getValue();
+				mostFrequentCapitalizedWord = entry.getKey();
+			}
+		}
+		retValue += mostFrequentWord + "," + mostFrequentCapitalizedWord + ",";
+		System.out.println(retValue);
 	}
 	
 	public static final void main(String[] args) throws IOException {
