5efd5224157af99c9528e000ff868667b54e546e
Leon <Leon@margale.de>
2022-06-29 21:13:33 +0200

    allmost done

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -1,9 +1,21 @@
 package idh.java;
 
 
+
+import org.apache.commons.csv.CSVFormat; 
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 import org.apache.commons.io.FileUtils;
@@ -28,12 +40,73 @@ public class Document implements Iterable<String> {
 	}
 	
 	public void printStats(File f) {
+			try {
+				
+				BufferedWriter writer = Files.newBufferedWriter(Paths.get(f.getAbsolutePath()));
+				ArrayList<String> wortliste = new ArrayList<String>();
+				Set<String> differentwords = new HashSet<String>();
+				this.getDocumentText();
+				this.iterator();
+				HashMap<String,Stack<String>> mostUsed = new HashMap<String,Stack<String>>();
+				int g = 0;
+				for(String token : this) {
+					wortliste.add(token);
+					differentwords.add(token);
+					if(!(mostUsed.containsKey(token))) {
+						mostUsed.put(token, new Stack<String>());
+						Stack<String> i = mostUsed.get(token);
+						
+						i.push(token);
+					}else {
+						
+						Stack<String> i = mostUsed.get(token);
+						i.push(token);
+					}
+					
+				}
+				int l = 0; 
+				int mostWord = 0;
+				String mostUsedWord  = ""; 
+			for(Stack<String> value : mostUsed.values()) {
+				l = value.size();
+				
+				if(l > mostWord) {
+					mostWord = l; 
+					mostUsedWord = value.get(0);
+				}
+				
+			}
+				
+				System.out.println(mostUsedWord);
+				int size = wortliste.size();				
+			 	long  smallerThan5 = wortliste.stream().filter(s -> s.length() < 5).count();
+			 	long blood =  wortliste.stream().filter(s -> s.contains("blood")).count();
+			 	int differentw = differentwords.size();
+			 	
+			 	
+			 	
+			 	
+			 	CSVPrinter csvprinter = new CSVPrinter(writer,CSVFormat.DEFAULT.withHeader(
+			 			"AnzahlDerWorte", "Worte < 5", "VerschiedeneWorte", "Am MeistenBenutzt", "wie oft blood vorkommt",
+			 			"das am häufigsten vorkommende großgeschriebene Wort")
+			 			);
+			 	
+			 	csvprinter.printRecord(size,smallerThan5, differentw,blood,mostUsedWord);
+			 	csvprinter.flush();
+			 	csvprinter.close();
+			 	
+			}catch(IOException e ) {
+				e.printStackTrace();
+			}
+		
+		return;
+		
 		// TODO: Implement
 	}
 	
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
-		d.printStats(new File("target/stats.csv"));
+		d.printStats(new File("bin/target/stats.csv"));
 	}
 
 	public Iterator<String> iterator() {
