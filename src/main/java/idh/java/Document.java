3642771e83cf0de8c2e3fd24731be161383b0a8f
Nils Reiter <nils.reiter@uni-koeln.de>
2022-06-29 11:16:06 +0200

    initial commit

---
+++ src/main/java/idh/java/Document.java
@@ -0,0 +1,58 @@
+package idh.java;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.commons.io.FileUtils;
+
+public class Document implements Iterable<String> {
+	String documentText;
+
+	public static Document readFromFile(File f) throws IOException {
+		
+		Document doc = new Document();
+		doc.documentText = FileUtils.readFileToString(f, "UTF-8");
+		
+		return doc;
+	}
+	
+	public String getDocumentText() {
+		return documentText;
+	}
+
+	public void setDocumentText(String documentText) {
+		this.documentText = documentText;
+	}
+	
+	public void printStats(File f) {
+		// TODO: Implement
+	}
+	
+	public static final void main(String[] args) throws IOException {
+		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
+		d.printStats(new File("target/stats.csv"));
+	}
+
+	public Iterator<String> iterator() {
+		return new Iterator<String>() {
+
+			StringTokenizer tokenizer = new StringTokenizer(documentText);
+			
+			@Override
+			public boolean hasNext() {
+				return tokenizer.hasMoreTokens();
+			}
+
+			@Override
+			public String next() {
+				return tokenizer.nextToken();
+			}
+			
+		};
+	}
+	
+	
+}
