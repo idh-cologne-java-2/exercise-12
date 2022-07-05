ea246d5d9ce35d720219bd10372d34d973d61840
UntoastedToast <UntoastedToast@users.noreply.github.com>
2022-07-05 16:10:13 +0200

    Task done

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -6,9 +6,14 @@ package idh.java;
  */
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 
@@ -31,8 +36,54 @@ public class Document implements Iterable<String> {
 		this.documentText = documentText;
 	}
 
-	public void printStats(File f) {
-		// TODO: Implement
+	public void printStats(File f) throws IOException {
+
+		f.createNewFile();
+		FileWriter fileWriter = new FileWriter(f);
+		fileWriter.append("Counted words," + "Number of types," + "Number of words shorter than 5 characters, "
+				+ "Frequency of the word \"blood\"," + "The most common word," + "The most common capitalized word"
+				+ "\n");
+		
+		ArrayList<String> arrayList = new ArrayList<String>();
+
+		for (String token : this) {
+			arrayList.add(token);
+		}
+
+		// Number of words
+		long totalWords = arrayList.stream().count();
+
+		// Number of types
+		long types = arrayList.stream().distinct().count();
+
+		// Number of words shorter than 5 characters
+		long shortWords = arrayList.stream().filter(w -> w.length() < 5).count();
+
+		// Frequency of the word "blood"
+		long blood = arrayList.stream().filter(b -> b.equals("blood")).count();
+
+		// The most common word
+		String mostCommonWord = arrayList.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
+				.max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+
+		// The most common capitalized word
+		String mostCommonCapitalizedWord = arrayList.stream()
+				.filter(u -> u.toCharArray()[0] == u.toUpperCase().toCharArray()[0])
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet().stream()
+				.max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+
+		fileWriter.append(totalWords + "," + types + "," + shortWords + "," + blood + "," + mostCommonWord + ","
+				+ mostCommonCapitalizedWord + "\n");
+		fileWriter.flush();
+		fileWriter.close();
+
+		System.out.println("Counted words: " + totalWords);
+		System.out.println("Number of types: " + types);
+		System.out.println("Number of words shorter than 5 characters: " + shortWords);
+		System.out.println("Frequency of the word \"blood\": " + blood);
+		System.out.println("The most common word: " + mostCommonWord);
+		System.out.println("The most common capitalized word: " + mostCommonCapitalizedWord);
 	}
 
 	public static final void main(String[] args) throws IOException {
