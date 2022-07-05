3ead9cef67d51a874dfcd02fa320263ef2a29ef6
Steffi S.-E. <steffix317@gmail.com>
2022-07-05 16:44:13 +0200

    Daten aus Dracula Text gefiltert

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -2,9 +2,17 @@ package idh.java;
 
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 
@@ -27,8 +35,46 @@ public class Document implements Iterable<String> {
 		this.documentText = documentText;
 	}
 	
-	public void printStats(File f) {
+	public void printStats(File f) throws IOException {
 		// TODO: Implement
+		f.createNewFile();
+		FileWriter fw = new FileWriter(f);
+		//benötigtes Dokument
+		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
+		//Liste die gefilterte Dinge speichert
+		ArrayList<String> filtered = new ArrayList<String>();
+		
+		for (String token : d) {
+			filtered.add(token);
+		}
+		
+		long allWords = filtered.stream().count();
+		long types = filtered.stream().distinct().count();
+		long shortTokens = filtered.stream().filter(s -> s.length() < 5).count();
+		long blood = filtered.stream().filter(b -> b.contains("blood")).count();
+		String mostFrequentWord = filtered.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+			      .entrySet()
+			      .stream()
+			      .max(Map.Entry.comparingByValue())
+			      .map(Map.Entry::getKey).orElse("");
+		String mostUppercaseWord = filtered.stream().filter(u -> u.toCharArray()[0] == u.toUpperCase().toCharArray()[0])
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				.entrySet()
+				.stream()
+				.max(Map.Entry.comparingByValue())
+				.map(Map.Entry::getKey).orElse("");
+		
+		
+		fw.append(allWords + ","+ types + ","+ shortTokens + "," + blood + "," + mostFrequentWord + "," + mostUppercaseWord + "\n");
+		
+		fw.flush();
+		fw.close();
+		System.out.println("Gesamtwörter: " + allWords);
+		System.out.println("Anzahl der Types: " + types);
+		System.out.println("Wörter unter 5 Zeichen: " + shortTokens);
+		System.out.println("Häufigkeit des Wortes Blut: " + blood);
+		System.out.println("Häufigstes Wort: " + mostFrequentWord);
+		System.out.println("Häufigstes großgeschriebenes Wort: " + mostUppercaseWord);
 	}
 	
 	public static final void main(String[] args) throws IOException {
