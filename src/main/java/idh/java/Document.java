694f3b3ed9d57370a64281126e693a0b09fcddd6
UntoastedToast <UntoastedToast@users.noreply.github.com>
2022-07-05 15:46:29 +0200

    Branch created

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -1,5 +1,9 @@
 package idh.java;
 
+/**
+ * @author UntoastedToast
+ *
+ */
 
 import java.io.File;
 import java.io.IOException;
@@ -12,13 +16,13 @@ public class Document implements Iterable<String> {
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
-		
+
 		Document doc = new Document();
 		doc.documentText = FileUtils.readFileToString(f, "UTF-8");
-		
+
 		return doc;
 	}
-	
+
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -26,11 +30,11 @@ public class Document implements Iterable<String> {
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
-	
+
 	public void printStats(File f) {
 		// TODO: Implement
 	}
-	
+
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
 		d.printStats(new File("target/stats.csv"));
@@ -40,7 +44,7 @@ public class Document implements Iterable<String> {
 		return new Iterator<String>() {
 
 			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
+
 			@Override
 			public boolean hasNext() {
 				return tokenizer.hasMoreTokens();
@@ -50,9 +54,8 @@ public class Document implements Iterable<String> {
 			public String next() {
 				return tokenizer.nextToken();
 			}
-			
+
 		};
 	}
-	
-	
+
 }
