b0f56441039489d16cbd7dfa718ec03763d99b1f
DKramer8 <103896137+DKramer8@users.noreply.github.com>
2022-07-06 15:03:32 +0200

    Einigermaßen eigenständig fertig

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -1,24 +1,32 @@
 package idh.java;
 
-
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.io.FileUtils;
 
 public class Document implements Iterable<String> {
 	String documentText;
 
 	public static Document readFromFile(File f) throws IOException {
-		
+
 		Document doc = new Document();
 		doc.documentText = FileUtils.readFileToString(f, "UTF-8");
-		
+
 		return doc;
 	}
-	
+
 	public String getDocumentText() {
 		return documentText;
 	}
@@ -26,11 +34,54 @@ public class Document implements Iterable<String> {
 	public void setDocumentText(String documentText) {
 		this.documentText = documentText;
 	}
-	
-	public void printStats(File f) {
-		// TODO: Implement
+
+	public void printStats(File f) throws IOException {
+
+		List<String> tokens = new ArrayList<String>();
+		for (String token : this)
+			tokens.add(token);
+		CSVPrinter p = new CSVPrinter(new FileWriter(f), CSVFormat.EXCEL);
+		p.printRecord("Anzahl der Wörter", "Anzahl der Types", "Anzahl der Wörter die kürzer sind als 5 Zeichen", "Häufigkeit des Wortes 'blood'", "Das am häufigsten vorkommende Wort",
+				"Das am häufigsten vorkommende großgeschriebene Wort");
+		p.print(tokens.stream().count());
+		p.print(tokens.stream().distinct().count());
+		p.print(tokens.stream().filter(s -> s.length() < 5).count());
+		p.print(tokens.stream().filter(s -> s.equals("blood")).count());
+
+		// Das am häufigsten vorkommende Wort
+		// Einfach aus der Musterlösung übernommen, da keinen eigenen Lösungsweg gefunden.
+		
+		String mft = null;
+		long frequency = 0;
+		Map<String, Long> ft = tokens.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		for (String token : ft.keySet()) {
+			if (ft.get(token) > frequency) {
+				frequency = ft.get(token);
+				mft = token;
+			}
+		}
+
+		p.print(mft);
+
+		// Das am häufigsten vorkommende großgeschriebene Wort
+		// Auch aus der Musterlösung übernommen; Nur dass '.isUpperCase' benutzt werden muss, war mir klar.
+		mft = null;
+		frequency = 0;
+		ft = tokens.stream().filter(s -> Character.isUpperCase(s.charAt(0)))
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+		
+		for (String token : ft.keySet()) {
+			if (ft.get(token) > frequency) {
+				frequency = ft.get(token);
+				mft = token;
+			}
+		}
+
+		p.print(mft);
+		p.close();
 	}
-	
+
 	public static final void main(String[] args) throws IOException {
 		Document d = Document.readFromFile(new File("src/main/resources/dracula.txt"));
 		d.printStats(new File("target/stats.csv"));
@@ -40,7 +91,7 @@ public class Document implements Iterable<String> {
 		return new Iterator<String>() {
 
 			StringTokenizer tokenizer = new StringTokenizer(documentText);
-			
+
 			@Override
 			public boolean hasNext() {
 				return tokenizer.hasMoreTokens();
@@ -50,9 +101,8 @@ public class Document implements Iterable<String> {
 			public String next() {
 				return tokenizer.nextToken();
 			}
-			
+
 		};
 	}
-	
-	
+
 }
