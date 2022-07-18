017fbc6d33ec6f89d3048e7dda1e03dea04b8f9a
jacekjackowski <jacekjackowski@192.168.2.112>
2022-07-18 23:16:40 +0200

    all done, i think

--- src/main/java/idh/java/Document.java
+++ src/main/java/idh/java/Document.java
@@ -5,6 +5,10 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
 
@@ -27,8 +31,30 @@ public class Document implements Iterable<String> {
 		this.documentText = documentText;
 	}
 	
-	public void printStats(File f) {
-		// TODO: Implement
+	public void printStats(File f) throws IOException {
+		
+		Document d = Document.readFromFile(f);
+		ArrayList<String> al = new ArrayList<String>();
+		
+		for(String token : d) {
+			al.add(token);
+		}
+		
+		long allwords = al.stream().count();
+		long types = al.stream().distinct().count();
+		long shorterthan5 = al.stream().filter(s -> s.length() < 5).count();
+		long blood = al.stream().filter(b -> b.contains("blood")).count();
+		String mostfrequent = al.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+				.entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+		String mostfrequentuc = al.stream().filter(u -> u.toCharArray()[0] == u.toUpperCase().toCharArray()[0])
+				.collect(Collectors.groupingBy(Function.identity(),
+				Collectors.counting())).entrySet().stream().max(Map.Entry.comparingByValue()).map(Map.Entry::getKey).orElse("");
+		
+		System.out.println("Anzahl der Wörter: " + allwords);
+		System.out.println("Anzahl der Types: " + types);
+		System.out.println("Anzahl der Wörter, die kürzer sind als 5 Zeichen: " + shorterthan5);
+		System.out.println("Am häufigsten vorkommendes Wort: " + mostfrequent);
+		System.out.println("Am häufigsten vorkommendes großgeschriebenes Wort: " + mostfrequentuc);
 	}
 	
 	public static final void main(String[] args) throws IOException {
